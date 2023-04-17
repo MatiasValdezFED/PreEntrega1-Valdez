2fe021dec04e7322cbe597a1d1e2b1bcdabb546c
Matias Valdez <matiasvaldez33@gmail.com>
2023-04-17 20:07:20 -0300

    Primera Parte

--- PreEntrega1.js
+++ PreEntrega1.js
@@ -1,1 +1,19 @@
-//Calculador de costo final de productos
+//Calculador de costo total de productos seleccionados por un usario
+
+alert("Le damos la bienvenida a Entre Hilos! por favor siga las instrucciones para realizar su compra. Click en 'Aceptar' para continuar")
+
+function ingresarNombre() {
+    const NOMBRE = prompt("Por favor ingrese su nombre")
+    if (NOMBRE == null || NOMBRE.trim() == "") {
+        alert("Por favor inserte un nombre válido para continuar")
+        return ingresarNombre();
+    } 
+    else {
+        alert(`Hola ${NOMBRE}! ahora le vamos a mostrar nuestro catalogo, por favor haga click en 'Aceptar' para continuar`)
+    }
+}
+
+ingresarNombre();
+
+
+
